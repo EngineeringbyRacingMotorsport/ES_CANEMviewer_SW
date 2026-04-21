bb0df0385d092327ae092c66faf8f49622038f25
alexcanas <1789389@campus.euss.org>
2026-04-21 13:13:39 +0200

    Añadir las pcbs en la pestaña general

--- CANEMviewer/CANEM_viewer.py
+++ CANEMviewer/CANEM_viewer.py
@@ -28,16 +28,57 @@ style.map(
     foreground=[("selected", "#0a594d"), ("!selected", "black")],
 )
 
-# --- NOTEBOOK (PESTANYES) ---
+# --- NOTEBOOK ---
 notebook = ttk.Notebook(ventana)
 notebook.pack(fill="both", expand=True)
 
-# Llista de pestanyes
-llista_finestres = ["General", "Temperatures", "SDC", "HV", "LV"]
+# --- PESTAÑAS ---
+general_frame = tk.Frame(notebook, bg="white")
+temperatures_frame = tk.Frame(notebook, bg="white")
+sdc_frame = tk.Frame(notebook, bg="white")
+hv_frame = tk.Frame(notebook, bg="white")
+lv_frame = tk.Frame(notebook, bg="white")
 
-# Crear pestanyes
-for nombre in llista_finestres:
-    frame = tk.Frame(notebook, bg="white")
-    notebook.add(frame, text=nombre)
+notebook.add(general_frame, text="General")
+notebook.add(temperatures_frame, text="Temperatures")
+notebook.add(sdc_frame, text="SDC")
+notebook.add(hv_frame, text="HV")
+notebook.add(lv_frame, text="LV")
+
+# --- TREEVIEWS GENERAL ---
+titulos = ["REAR ECU", "HVAB", "HVDB", "TSAL GREEN", "SDC RESET"]
+
+# layout grid centrado
+for col in range(6):
+    general_frame.columnconfigure(col, weight=1)
+
+general_frame.rowconfigure(0, weight=1)
+general_frame.rowconfigure(1, weight=1)
+
+trees = []
+
+for titulo in titulos:
+    tree = ttk.Treeview(
+        general_frame,
+        columns=(titulo,),
+        show="headings",
+        height=1
+    )
+
+    tree.heading(titulo, text=titulo)
+    tree.column(titulo, width=160, anchor="center")
+
+    trees.append(tree)
+
+# --- POSICIÓN (2 arriba / 3 abajo) ---
+
+# arriba
+trees[0].grid(row=0, column=2, pady=40)
+trees[1].grid(row=0, column=4, pady=40)
+
+# abajo
+trees[2].grid(row=1, column=1, pady=40)
+trees[3].grid(row=1, column=3, pady=40)
+trees[4].grid(row=1, column=5, pady=40)
 
 ventana.mainloop()
