041acf5c483a7ba759265a8acc72c17499072857
alexcanas <1789389@campus.euss.org>
2026-04-28 22:49:38 +0200

    Part diccionari provisional

---
+++ CANEMviewer/dicc.py
@@ -0,0 +1,171 @@
+dades = ¨{
+    "frontECU1": {
+        "FpANLRpot": {
+            "valor": 124,
+            "unitat": %,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpANLLpot": {
+            "valor": 124,
+            "unitat": %,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpANLRsus": {
+            "valor": 124,
+            "unitat": mm,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpANLLsus": {
+            "valor": 124,
+            "unitat": mm,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpDIGRvel": {
+            "valor": 124,
+            "unitat": km/h,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpDIGLvel": {
+            "valor": 124,
+            "unitat": km/h,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpANLbrake": {
+            "valor": 124,
+            "unitat": MPa,
+            "valormax": 12,
+            "valormin": 0
+        }                   
+    }
+    "frontECU2": {
+        "FpINTebms": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpINTeimd": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpINTtsoff": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpINTsbms": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpINTpre": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpINTr2d": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpINTmenu": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpDIGmicrosd": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpSDCinertia": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpSDCbots": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpSDCcsdb": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpERRapps": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpDIGrefri": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpDIGr2d": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpDIGvel": {
+            "valor": 124,
+            "unitat": km/h ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpANLtaccu": {
+            "valor": 124,
+            "unitat": ºC,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpANLvaccu": {
+            "valor": 124,
+            "unitat": V,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "FpSHU": {
+            "valor": 124,
+            "unitat": mA,
+            "valormax": 12,
+            "valormin": 0
+        }
+    }
+    "HVAB": {
+        "ApTHRhv": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "ApSHU": {
+            "valor": 124,
+            "unitat": mA,
+            "valormax": 12,
+            "valormin": 0
+        }  
+          
+    }       
+} 
