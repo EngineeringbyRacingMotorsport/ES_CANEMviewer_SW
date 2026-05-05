555ee934a5520b8cb7126f714d52dec53819e181
alexcanas <1789389@campus.euss.org>
2026-05-05 09:46:37 +0200

    Finalitzat el diccionari a falta dels valors max i min

--- CANEMviewer/dicc.py
+++ CANEMviewer/dicc.py
@@ -153,6 +153,130 @@ dades = ¨{
             "valormin": 0
         }
     }
+    "rearECU1": {
+        "RpSIGRsus": {
+            "valor": 124,
+            "unitat": mm,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "RpSIGLsus": {
+            "valor": 124,
+            "unitat": mm,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "RpSIGRspeed": {
+            "valor": 124,
+            "unitat": km/h,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "RpSIGLspeed": {
+            "valor": 124,
+            "unitat": km/h,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "RpSIGItempM": {
+            "valor": 124,
+            "unitat": ºC,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "RpSIGOtempM": {
+            "valor": 124,
+            "unitat": ºC,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "RpSIGItempI": {
+            "valor": 124,
+            "unitat": ºC,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "RpSIGOtempI": {
+            "valor": 124,
+            "unitat": ºC,
+            "valormax": 12,
+            "valormin": 0
+        }
+    }
+    "rearECU2": {
+        "RpSDChvd": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "RpSDCtsms": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "RpSDCrsdb": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "RpSDClsdb": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "RpSTAbrkledR": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "RpSTAbrkledG": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "RpSTAbrkledB": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "RpSTArefriaccu": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "RpSTArefrimot": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "RpSTArefriinv": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "RpSIGlvs": {
+            "valor": 124,
+            "unitat": V,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "RpSHU": {
+            "valor": 124,
+            "unitat": mA,
+            "valormax": 12,
+            "valormin": 0
+        }   
+    }
     "HVAB": {
         "ApTHRhv": {
             "valor": 124,
@@ -167,5 +291,197 @@ dades = ¨{
             "valormin": 0
         }  
           
+    }
+    "HVDB": {
+        "BTHRbrake": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "BTHRcurrent": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "BERRplaus": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "BERRtimer": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "BSDC": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "DpSDC": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "DTHRhv": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "DLCHdischarge": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "DSDCintlck1": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "DSDCintlck2": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "BSHU": {
+            "valor": 124,
+            "unitat": mA,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "DSHU": {
+            "valor": 124,
+            "unitat": mA,
+            "valormax": 12,
+            "valormin": 0
+        } 
+    }
+    "TSAL Green": {
+        "TpDIGspre": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "TpDIGsairp": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "TpDIGsairn": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "TpDIGipre": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "TpDIGiairp": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "TpDIGiairn": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "TpTHRhv": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "TpERRscs": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "TpTHRdis": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "TpLCH": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "TpINTled": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+    }
+    "SDC Reset": {
+        "SpERRbms": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "SpERRimd": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "SpLCHebms": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "SpLCHeimd": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "SpINTresbut": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "SpSDCbms": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "SpSDCimd": {
+            "valor": 124,
+            "unitat": ,
+            "valormax": 12,
+            "valormin": 0
+        }
+        "SpSHU": {
+            "valor": 124,
+            "unitat": mA,
+            "valormax": 12,
+            "valormin": 0
+        }
     }       
 } 
